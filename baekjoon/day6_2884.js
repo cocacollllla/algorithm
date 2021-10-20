6abb6cd84ea23b4a59e93eb09464d5e97b85dd44
cocacollllla <cocacollllla@gmail.com>
2021-10-20 15:03:25 +0900

    day6 algorithm

---
+++ baekjoon/day6_2884.js
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/2884
+
+var fs = require('fs');
+var input = fs.readFileSync('text.txt').toString().split(' ');
+var hour = parseInt(input[0]);
+var min = parseInt(input[1]);
+
+if(min < 45){
+    console.log(hour === 0 ? hour = 23 : hour-=1, min+=15);
+}else{
+    console.log(hour, min-=45);
+}
+
+
+
