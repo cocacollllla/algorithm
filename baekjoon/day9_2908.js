8b3f8175da9cf27cdada564c512357f50b8eb1bc
cocacollllla <cocacollllla@gmail.com>
2021-10-23 22:52:11 +0900

    day9 algorithm

---
+++ baekjoon/day9_2908.js
@@ -0,0 +1,11 @@
+https://www.acmicpc.net/problem/2908
+
+const input = require('fs').readFileSync('text.txt').toString().split(' ');
+
+let newArr = [];
+for(let i=0; i<input.length; i++){
+    let tesz = input[i].split('').reverse().join('');
+    newArr.push(parseInt(tesz));
+}
+
+console.log(newArr[0] > newArr[1] ? newArr[0] : newArr[1]);
